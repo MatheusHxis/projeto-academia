ba5b46ca2e46b3dbfe624437baac41c659a92e2e
Matheus Henrique <152836672+MatheusHxis@users.noreply.github.com>
2024-01-12 10:24:33 -0300

    php simples

--- php/config.php
+++ php/config.php
@@ -1,8 +1,8 @@
 <?php
-    $dbhost = "";
-    $dbusername = "";
-    $dbpassword = "";
-    $dbname = "";
+    $dbhost = "database-2.c7gg06msoq51.us-east-2.rds.amazonaws.com";
+    $dbusername = "admin";
+    $dbpassword = "jon1092387456";
+    $dbname = "academia";
 
     $conn = new mysqli($dbhost, $dbusername, $dbpassword, $dbname);
     if ($conn->connect_error) {
