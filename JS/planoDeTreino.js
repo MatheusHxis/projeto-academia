397c2dac4ce9e745a14571685f159ffcb2e5f6e0
Matheus Henrique <152836672+MatheusHxis@users.noreply.github.com>
2024-01-17 10:48:09 -0300

    lista de treino

---
+++ JS/planoDeTreino.js
@@ -0,0 +1,20 @@
+function addExercicio(){
+    let addTreino = document.getElementById("exercicio").value
+    let reps = document.getElementById("reps").value
+    let kg = document.getElementById("kg").value
+
+    if (addTreino && reps && kg){
+        let novoExercicio = document.createElement("li")
+            novoExercicio.textContent = `${addTreino} | Peso: ${kg} | reps: ${reps}`;
+        document.getElementById('listaDeTreino').appendChild(novoExercicio)
+
+
+        // codigo para limpa o formulario
+        
+        document.getElementById('addTreino').value = ""
+        document.getElementById('reps').value = ""
+        document.getElementById('kg').value = ""
+    } else{
+        alert('Por favor, preencha todos os campos.')
+    }
+}
