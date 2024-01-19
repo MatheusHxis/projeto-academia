578fef0e81e0a7e74dc8006159efe82c69eb0443
Matheus Henrique <152836672+MatheusHxis@users.noreply.github.com>
2024-01-19 10:29:59 -0300

    teste de lista

--- JS/planoDeTreino.js
+++ JS/planoDeTreino.js
@@ -1,25 +1,34 @@
-function addExercicio(){
+function addExercicio() {
     let addTreino = document.getElementById("exercicio").value
     let reps = document.getElementById("reps").value
     let kg = document.getElementById("kg").value
+    let diasDaSemana = document.getElementById('diasDaSemana').value
+    
 
-    if (addTreino && reps && kg){
-        let novoExercicio = document.createElement("li")
-            novoExercicio.textContent = `${addTreino} | Peso: ${kg} | reps: ${reps}`;
-        document.getElementById('listaDeTreino').appendChild(novoExercicio)
+    if (addTreino && reps && kg) {
+        let novoExercicioTexto = document.createElement("li")
+        novoExercicioTexto.textContent = `${addTreino} | Peso: ${kg} | reps: ${reps}`;
+        
+        document.getElementById('listaDeTreino').appendChild(novoExercicioTexto)
 
+
+        let novoExercicio = document.createElement('li')
+
+        let textoExercicio = document.createElement('span');
+        textoExercicio.textContent = novoExercicioTexto
+
+        novoExercicio.appendChild(textoExercicio)
+
+        let listaDeExerciciosDia = document.getElementById(`listaDeExercicios -${diasDaSemana}`)
+
+        listaDeExerciciosDia.appendChild(novoExercicio)
 
         // codigo para limpa o formulario
-        
+
         document.getElementById('addTreino').value = ""
         document.getElementById('reps').value = ""
         document.getElementById('kg').value = ""
-    } else{
+    } else {
         alert('Por favor, preencha todos os campos.')
     }
 }
-document.getElementById('diasDaSemana').addEventListener('change', function(){
-    let diaSelecionado = this
-}
-
-)
